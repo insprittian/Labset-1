aad69d8e249d342558756adce4ef44690129289e
insprittian <61277701+insprittian@users.noreply.github.com>
2021-06-01 08:58:54 +1200

    Part 5 Completed

---
+++ Labset 1/Labset 1/Controllers/PostsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Labset_1.Models;
+
+namespace Labset_1.Controllers
+{
+    public class PostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Get: Create Post View
+        // GET Create View
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Post: Create Post
+        // Post Create Post
+        [HttpPost]
+        public IActionResult Create(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(post);
+            }
+            post.PostedDate = DateTime.Now;
+            post.Author = User.Identity.Name;
+
+            return View();
+        }
+
+        #endregion
+
+
+        #region Post
+        public IActionResult Post(int id)
+        {
+            Post post = new Post();
+            post.Title = "My Blog Post";
+            post.PostedDate = DateTime.Now;
+            post.Author = "Sam Zhao";
+            post.Body = "This is my first blogpost";
+            return View(post);
+        }
+
+        #endregion
+    }
+}
