4e1c0136b03e2b63622ea293cc83e50cfa3e37d6
insprittian <61277701+insprittian@users.noreply.github.com>
2021-06-01 07:32:21 +1200

    Part 3 Completed

---
+++ Labset 1/Labset 1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labset_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello, ASP.NET MVC!";
+        }
+
+
+        public int Echo(int id)
+        {
+            return id;
+        }
+
+    }
+}
